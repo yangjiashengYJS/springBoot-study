950136a67f66cc3c04517a80fa0f24904d33b387
jason_YJS <596185079@qq.com>
2017-11-24 11:04:06 +0800

    update

--- Chapter4-5-1/src/test/java/com/didispace/ApplicationTests.java
+++ Chapter4-5-1/src/test/java/com/didispace/ApplicationTests.java
@@ -31,8 +31,8 @@ public class ApplicationTests {
 	public void sendSimpleMail() throws Exception {
 
 		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom("dyc87112@qq.com");
-		message.setTo("dyc87112@qq.com");
+		message.setFrom("596185079@qq.com");
+		message.setTo("350412315@qq.com");
 		message.setSubject("主题：简单邮件");
 		message.setText("测试邮件内容");
 
@@ -45,12 +45,12 @@ public class ApplicationTests {
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 
 		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-		helper.setFrom("dyc87112@qq.com");
-		helper.setTo("dyc87112@qq.com");
+		helper.setFrom("596185079@qq.com");
+		helper.setTo("350412315@qq.com");
 		helper.setSubject("主题：有附件");
 		helper.setText("有附件的邮件");
 
-		FileSystemResource file = new FileSystemResource(new File("weixin.jpg"));
+		FileSystemResource file = new FileSystemResource(new File("yjs.jpg"));
 		helper.addAttachment("附件-1.jpg", file);
 		helper.addAttachment("附件-2.jpg", file);
 
@@ -63,12 +63,12 @@ public class ApplicationTests {
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 
 		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-		helper.setFrom("dyc87112@qq.com");
-		helper.setTo("dyc87112@qq.com");
+		helper.setFrom("596185079@qq.com");
+		helper.setTo("350412315@qq.com");
 		helper.setSubject("主题：嵌入静态资源");
 		helper.setText("<html><body><img src=\"cid:weixin\" ></body></html>", true);
 
-		FileSystemResource file = new FileSystemResource(new File("weixin.jpg"));
+		FileSystemResource file = new FileSystemResource(new File("yjs.jpg"));
 		helper.addInline("weixin", file);
 
 		mailSender.send(mimeMessage);
@@ -80,8 +80,8 @@ public class ApplicationTests {
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 
 		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-		helper.setFrom("dyc87112@qq.com");
-		helper.setTo("dyc87112@qq.com");
+		helper.setFrom("596185079@qq.com");
+		helper.setTo("350412315@qq.com");
 		helper.setSubject("主题：模板邮件");
 
 		Map<String, Object> model = new HashedMap();
